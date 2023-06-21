e13ee691e3a11884e42819ddd4be0ac3d8e5dd44
dooxsav <dooxsav@gmail.com>
2023-06-21 11:52:43 +0200

    Mise en place du frontend

--- START.bat
+++ START.bat
@@ -2,7 +2,12 @@
 
 REM METTRE EN MEMOIRE LE CHEMIN OU SE TROUVE LE FICHIER
 set BIONEST_API_PATH=%~dp0\API
+set BIONEST_FRONTEND_PATH=%~dp0\frontend
+
 echo %BIONEST_API_PATH%
+echo %BIONEST_FRONTEND_PATH%
+
+REM -------------------------------------------- REM
 
 REM Démarrer l'API_USERS
 cd %BIONEST_API_PATH%\API_USERS
@@ -19,5 +24,14 @@ REM Attendre 5 secondes
 timeout /t 5
 
 REM Démarrer l'API_PROD
-cd %BIONEST_API_PATH%\API_SECURITY
+cd %BIONEST_API_PATH%\API_PRODUCTION
 start cmd /k "SET DEBUG=api-production:* & npm start"
+
+REM ------------------------------------------------- REM
+
+REM Attendre 5 secondes
+timeout /t 5
+
+REM Démarrer la partie frontend
+cd %BIONEST_FRONTEND_PATH%
+start cmd /k "npm start"
